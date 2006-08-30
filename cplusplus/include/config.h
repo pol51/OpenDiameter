ff48c45199f06084c736eed981a276e8020a117c
vfajardo <vfajardo@c7ec899b-d015-0410-9a68-a346fd343c46>
2006-08-30 14:27:23 +0000

    Fixes to autogen scripts
    git-svn-id: svn://svn.code.sf.net/p/diameter/svn@93 c7ec899b-d015-0410-9a68-a346fd343c46

---
+++ cplusplus/include/config.h
@@ -0,0 +1,63 @@
+/* BEGIN_COPYRIGHT                                                        */
+/*                                                                        */
+/* Open Diameter: Open-source software for the Diameter and               */
+/*                Diameter related protocols                              */
+/*                                                                        */
+/* Copyright (C) 2002-2006 Open Diameter Project                          */
+/*                                                                        */
+/* This library is free software; you can redistribute it and/or modify   */
+/* it under the terms of the GNU Lesser General Public License as         */
+/* published by the Free Software Foundation; either version 2.1 of the   */
+/* License, or (at your option) any later version.                        */
+/*                                                                        */
+/* This library is distributed in the hope that it will be useful,        */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of         */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU      */
+/* Lesser General Public License for more details.                        */
+/*                                                                        */
+/* You should have received a copy of the GNU Lesser General Public       */
+/* License along with this library; if not, write to the Free Software    */
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307    */
+/* USA.                                                                   */
+/*                                                                        */
+/* In addition, when you copy and redistribute some or the entire part of */
+/* the source code of this software with or without modification, you     */
+/* MUST include this copyright notice in each copy.                       */
+/*                                                                        */
+/* If you make any changes that are appeared to be useful, please send    */
+/* sources that include the changed part to                               */
+/* diameter-developers@lists.sourceforge.net so that we can reflect your  */
+/* changes to one unified version of this software.                       */
+/*                                                                        */
+/* END_COPYRIGHT                                                          */
+
+/* Diameter peer fsm debugging */
+#define AAA_FSM_EVENT_DEBUG 0
+
+/* Diameter router debugging */
+#define AAA_ROUTE_DEBUG 0
+
+/* Diameter acct session fsm debugging */
+#define AAA_SESSION_ACCT_DEBUG 0
+
+/* Diameter auth session fsm debugging */
+#define AAA_SESSION_AUTH_DEBUG 0
+
+/* Diameter generic session fsm debugging */
+#define AAA_SESSION_DEBUG 0
+
+/* Diameter XML config loader debugging */
+#define AAA_XML_DEBUG 0
+
+/* PANA cookie debugging flag */
+#define PANA_COOKIE_DEBUG 0
+
+/* PANA debugging flag */
+#define PANA_DEBUG 0
+
+/* PANA dhcp key generator debugging flag */
+#define PANA_DHCP_DEBUG 0
+
+/* PANA key generator debugging flag */
+#define PANA_SA_DEBUG 0
+
